17cd4c1fbf28d144c9553bdd05bc74cfc052015e
keiteio <mss.ymns@gmail.com>
2013-05-03 09:52:45 +0900

    commit

---
+++ Blackbuck/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Blackbuck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.FixPosition_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FixZoom_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.SavePosition_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Save1_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Save2_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Save3_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Save4_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Save5_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LoadPosition_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Load1_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Load2_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Load3_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Load4_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Load5_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.Close_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FixOpscity_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FixPosition_ToolStripMenuItem,
+            this.FixZoom_ToolStripMenuItem,
+            this.FixOpscity_ToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.SavePosition_ToolStripMenuItem,
+            this.LoadPosition_ToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.Close_ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(179, 170);
+            // 
+            // FixPosition_ToolStripMenuItem
+            // 
+            this.FixPosition_ToolStripMenuItem.CheckOnClick = true;
+            this.FixPosition_ToolStripMenuItem.Name = "FixPosition_ToolStripMenuItem";
+            this.FixPosition_ToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.FixPosition_ToolStripMenuItem.Text = "位置を固定(&S)";
+            this.FixPosition_ToolStripMenuItem.Click += new System.EventHandler(this.FixPosition_ToolStripMenuItem_Click);
+            // 
+            // FixZoom_ToolStripMenuItem
+            // 
+            this.FixZoom_ToolStripMenuItem.CheckOnClick = true;
+            this.FixZoom_ToolStripMenuItem.Name = "FixZoom_ToolStripMenuItem";
+            this.FixZoom_ToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.FixZoom_ToolStripMenuItem.Text = "サイズを固定(&Z)";
+            this.FixZoom_ToolStripMenuItem.Click += new System.EventHandler(this.FixZoom_ToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(175, 6);
+            // 
+            // SavePosition_ToolStripMenuItem
+            // 
+            this.SavePosition_ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Save1_ToolStripMenuItem,
+            this.Save2_ToolStripMenuItem,
+            this.Save3_ToolStripMenuItem,
+            this.Save4_ToolStripMenuItem,
+            this.Save5_ToolStripMenuItem});
+            this.SavePosition_ToolStripMenuItem.Name = "SavePosition_ToolStripMenuItem";
+            this.SavePosition_ToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.SavePosition_ToolStripMenuItem.Text = "位置/サイズを記録";
+            // 
+            // Save1_ToolStripMenuItem
+            // 
+            this.Save1_ToolStripMenuItem.Name = "Save1_ToolStripMenuItem";
+            this.Save1_ToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.Save1_ToolStripMenuItem.Text = "記録１(&1)";
+            this.Save1_ToolStripMenuItem.Click += new System.EventHandler(this.Save1_ToolStripMenuItem_Click);
+            // 
+            // Save2_ToolStripMenuItem
+            // 
+            this.Save2_ToolStripMenuItem.Name = "Save2_ToolStripMenuItem";
+            this.Save2_ToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.Save2_ToolStripMenuItem.Text = "記録２(&2)";
+            this.Save2_ToolStripMenuItem.Click += new System.EventHandler(this.Save2_ToolStripMenuItem_Click);
+            // 
+            // Save3_ToolStripMenuItem
+            // 
+            this.Save3_ToolStripMenuItem.Name = "Save3_ToolStripMenuItem";
+            this.Save3_ToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.Save3_ToolStripMenuItem.Text = "記録３(&3)";
+            this.Save3_ToolStripMenuItem.Click += new System.EventHandler(this.Save3_ToolStripMenuItem_Click);
+            // 
+            // Save4_ToolStripMenuItem
+            // 
+            this.Save4_ToolStripMenuItem.Name = "Save4_ToolStripMenuItem";
+            this.Save4_ToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.Save4_ToolStripMenuItem.Text = "記録４(&4)";
+            this.Save4_ToolStripMenuItem.Click += new System.EventHandler(this.Save4_ToolStripMenuItem_Click);
+            // 
+            // Save5_ToolStripMenuItem
+            // 
+            this.Save5_ToolStripMenuItem.Name = "Save5_ToolStripMenuItem";
+            this.Save5_ToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.Save5_ToolStripMenuItem.Text = "記録５(&5)";
+            this.Save5_ToolStripMenuItem.Click += new System.EventHandler(this.Save5_ToolStripMenuItem_Click);
+            // 
+            // LoadPosition_ToolStripMenuItem
+            // 
+            this.LoadPosition_ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Load1_ToolStripMenuItem,
+            this.Load2_ToolStripMenuItem,
+            this.Load3_ToolStripMenuItem,
+            this.Load4_ToolStripMenuItem,
+            this.Load5_ToolStripMenuItem});
+            this.LoadPosition_ToolStripMenuItem.Name = "LoadPosition_ToolStripMenuItem";
+            this.LoadPosition_ToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.LoadPosition_ToolStripMenuItem.Text = "位置/サイズを呼出";
+            // 
+            // Load1_ToolStripMenuItem
+            // 
+            this.Load1_ToolStripMenuItem.Name = "Load1_ToolStripMenuItem";
+            this.Load1_ToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.Load1_ToolStripMenuItem.Text = "記録１(&1)";
+            this.Load1_ToolStripMenuItem.Click += new System.EventHandler(this.Load1_ToolStripMenuItem_Click);
+            // 
+            // Load2_ToolStripMenuItem
+            // 
+            this.Load2_ToolStripMenuItem.Name = "Load2_ToolStripMenuItem";
+            this.Load2_ToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.Load2_ToolStripMenuItem.Text = "記録２(&2)";
+            this.Load2_ToolStripMenuItem.Click += new System.EventHandler(this.Load2_ToolStripMenuItem_Click);
+            // 
+            // Load3_ToolStripMenuItem
+            // 
+            this.Load3_ToolStripMenuItem.Name = "Load3_ToolStripMenuItem";
+            this.Load3_ToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.Load3_ToolStripMenuItem.Text = "記録３(&3)";
+            this.Load3_ToolStripMenuItem.Click += new System.EventHandler(this.Load3_ToolStripMenuItem_Click);
+            // 
+            // Load4_ToolStripMenuItem
+            // 
+            this.Load4_ToolStripMenuItem.Name = "Load4_ToolStripMenuItem";
+            this.Load4_ToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.Load4_ToolStripMenuItem.Text = "記録４(&4)";
+            this.Load4_ToolStripMenuItem.Click += new System.EventHandler(this.Load4_ToolStripMenuItem_Click);
+            // 
+            // Load5_ToolStripMenuItem
+            // 
+            this.Load5_ToolStripMenuItem.Name = "Load5_ToolStripMenuItem";
+            this.Load5_ToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.Load5_ToolStripMenuItem.Text = "記録５(&5)";
+            this.Load5_ToolStripMenuItem.Click += new System.EventHandler(this.Load5_ToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(175, 6);
+            // 
+            // Close_ToolStripMenuItem
+            // 
+            this.Close_ToolStripMenuItem.Name = "Close_ToolStripMenuItem";
+            this.Close_ToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.Close_ToolStripMenuItem.Text = "終了(&X)";
+            this.Close_ToolStripMenuItem.Click += new System.EventHandler(this.Close_ToolStripMenuItem_Click);
+            // 
+            // FixOpscity_ToolStripMenuItem
+            // 
+            this.FixOpscity_ToolStripMenuItem.CheckOnClick = true;
+            this.FixOpscity_ToolStripMenuItem.Name = "FixOpscity_ToolStripMenuItem";
+            this.FixOpscity_ToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.FixOpscity_ToolStripMenuItem.Text = "不透明度を固定(&X)";
+            this.FixOpscity_ToolStripMenuItem.Click += new System.EventHandler(this.FixOpscity_ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(594, 456);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Close_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FixPosition_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SavePosition_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Save1_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Save2_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Save3_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Save4_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Save5_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem LoadPosition_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem Load1_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Load2_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Load3_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Load4_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Load5_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem FixZoom_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FixOpscity_ToolStripMenuItem;
+    }
+}
+
---
+++ Blackbuck/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Blackbuck
+{
+    public partial class Form1 : Form
+    {
+        string[] FileExtentions = new string[] { ".jpg", ".jpeg", ".png" };
+
+        private System.Drawing.Point mousePoint;
+        private int oldMouseX;
+        private int oldMouseY;
+
+        private float zoom = 1.0f;
+
+        bool stay = false;
+        bool fixZoom = false;
+        bool fixOpacity = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Zoom = 1.0f;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        void Form1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// マウスが押された時に呼び出されるイベント
+        /// </summary>
+        void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+            if (e.Button == MouseButtons.Left)
+            {
+                mousePoint = new System.Drawing.Point(e.X, e.Y);
+            }
+        }
+
+        /// <summary>
+        /// マウスが動かされた時に呼び出されるイベント
+        /// </summary>
+        void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int deltaX = e.X - oldMouseX;
+            int deltaY = e.Y - oldMouseY;
+
+            float value = 0;
+            if (deltaX > deltaY)
+            {
+                value = deltaX;
+            }
+            else
+            {
+                value = deltaY;
+            }
+
+            if (e.Button == MouseButtons.Left)
+            {
+                if (!this.stay && !IsModifierKeyPressed(Keys.Shift) && !IsModifierKeyPressed(Keys.Control))
+                {
+                    this.Left += e.X - mousePoint.X;
+                    this.Top += e.Y - mousePoint.Y;
+                }
+                else if (!this.fixZoom && IsModifierKeyPressed(Keys.Control))
+                {
+                    this.Zoom = Math.Max(0.1f, this.Zoom + (value / 5000.0f));
+
+                }
+                else if (!this.fixOpacity && IsModifierKeyPressed(Keys.Shift))
+                {
+                    this.Opacity = Math.Max(0.1, this.Opacity + (value / 1000.0));
+                }
+            }
+
+            oldMouseX = e.X;
+            oldMouseY = e.Y;
+
+            this.Invalidate();
+        }
+
+        bool IsModifierKeyPressed(Keys key)
+        {
+            return (Control.ModifierKeys & key) == key;
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            string[] fileNameArray = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            if (!e.Data.GetDataPresent(System.Windows.Forms.DataFormats.FileDrop))
+            {
+                /* ファイル以外のドラッグは受け入れない */
+                e.Effect = DragDropEffects.None;
+                return;
+            }
+
+            if (fileNameArray.Length > 1)
+            {
+                /* 複数ファイルのドラッグは受け入れない */
+                e.Effect = DragDropEffects.None;
+                return;
+            }
+
+            if (!IsShowableFile(fileNameArray[0]))
+            {
+                /* 拡張子が.txt以外は受け入れない */
+                e.Effect = DragDropEffects.None;
+                return;
+            }
+
+            /* 上記以外は受け入れる */
+            e.Effect = DragDropEffects.All;
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] fileNameArray = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            string fileName = fileNameArray[0];
+
+            this.UseWaitCursor = true;
+            Image img = Image.FromFile(fileName);
+            this.UseWaitCursor = false;
+
+            this.BackgroundImage = img;
+
+            this.Zoom = 1.0f;
+        }
+
+        bool IsShowableFile(string filepath)
+        {
+            foreach(string ext in FileExtentions){
+                if (Path.GetExtension(filepath).ToLower() == ext)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void Close_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private float Zoom
+        {
+            get
+            {
+                return zoom;
+            }
+
+            set
+            {
+                if (this.BackgroundImage != null)
+                {
+                    int w = (int)(this.BackgroundImage.Width * value);
+                    int h = (int)(this.BackgroundImage.Height * value);
+                    Size newSize = new Size(w, h);
+                    this.ClientSize = newSize;
+                }
+                zoom = value;
+            }
+        }
+
+        private void FixPosition_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.stay = (false == this.stay);
+        }
+
+        private void FixZoom_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.fixZoom = (false == this.fixZoom);
+        }
+
+        private void FixOpscity_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.fixOpacity = (false == this.fixOpacity);
+        }
+
+        private void SavePosition(int num)
+        {
+            switch (num)
+            {
+                case 1:
+                    {
+                        Properties.Settings.Default.Save1Position = this.Location;
+                        Properties.Settings.Default.Save1Zoom = this.Zoom;
+                        break;
+                    }
+                case 2:
+                    {
+                        Properties.Settings.Default.Save2Position = this.Location;
+                        Properties.Settings.Default.Save2Zoom = this.Zoom;
+                        break;
+                    }
+                case 3:
+                    {
+                        Properties.Settings.Default.Save3Position = this.Location;
+                        Properties.Settings.Default.Save3Zoom = this.Zoom;
+                        break;
+                    }
+                case 4:
+                    {
+                        Properties.Settings.Default.Save4Position = this.Location;
+                        Properties.Settings.Default.Save4Zoom = this.Zoom;
+                        break;
+                    }
+                case 5:
+                    {
+                        Properties.Settings.Default.Save5Position = this.Location;
+                        Properties.Settings.Default.Save5Zoom = this.Zoom;
+                        break;
+                    }
+
+            }
+            Properties.Settings.Default.Save();
+        }
+
+        private void Save1_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SavePosition(1);
+        }
+
+        private void Save2_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SavePosition(2);
+        }
+
+        private void Save3_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SavePosition(3);
+        }
+
+        private void Save4_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SavePosition(4);
+        }
+
+        private void Save5_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SavePosition(5);
+        }
+
+        private void LoadPosition(int num)
+        {
+            Properties.Settings.Default.Reload();
+            switch (num)
+            {
+                case 1:
+                    {
+                        this.Location = Properties.Settings.Default.Save1Position;
+                        this.Zoom = Properties.Settings.Default.Save1Zoom;
+                        break;
+                    }
+                case 2:
+                    {
+                        this.Location = Properties.Settings.Default.Save2Position;
+                        this.Zoom = Properties.Settings.Default.Save2Zoom;
+                        break;
+                    }
+                case 3:
+                    {
+                        this.Location = Properties.Settings.Default.Save3Position;
+                        this.Zoom = Properties.Settings.Default.Save3Zoom;
+                        break;
+                    }
+                case 4:
+                    {
+                        this.Location = Properties.Settings.Default.Save4Position;
+                        this.Zoom = Properties.Settings.Default.Save4Zoom;
+                        break;
+                    }
+                case 5:
+                    {
+                        this.Location = Properties.Settings.Default.Save5Position;
+                        this.Zoom = Properties.Settings.Default.Save5Zoom;
+                        break;
+                    }
+            }
+        }
+
+        private void Load1_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoadPosition(1);
+        }
+
+        private void Load2_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoadPosition(2);
+        }
+
+        private void Load3_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoadPosition(3);
+        }
+
+        private void Load4_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoadPosition(4);
+        }
+
+        private void Load5_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoadPosition(5);
+        }
+
+    }
+
+}
